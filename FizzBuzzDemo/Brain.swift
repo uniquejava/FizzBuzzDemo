bbfcfe7d65cd6c67b8c85dd03885a0a674d06013
cyper <345343747@qq.com>
2016-11-09 00:42:58 +0800

    fix the error

---
+++ FizzBuzzDemo/Brain.swift
@@ -0,0 +1,15 @@
+//
+//  Brain.swift
+//  FizzBuzzDemo
+//
+//  Created by cyper on 09/11/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+import Foundation
+
+class Brain: NSObject {
+    func isDivisibleByThree(_ number: Int) -> Bool {
+        return true
+    }
+}
