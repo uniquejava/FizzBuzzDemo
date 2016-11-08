b68290504b8abcf10c63106ef9dc2ba07aee84de
cyper <345343747@qq.com>
2016-11-09 00:14:41 +0800

    write a test case first.

---
+++ FizzBuzzDemoTests/BrainTests.swift
@@ -0,0 +1,29 @@
+//
+//  BrainTests.swift
+//  FizzBuzzDemo
+//
+//  Created by cyper on 09/11/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+import XCTest
+
+class BrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(3)
+        XCTAssertEqual(result, true)
+    }
+    
+}
